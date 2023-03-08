db4a60d5df67e95b1adce43028bce656154fd3ed
William Poon <willphk@outlook.com>
2023-03-08 12:14:03 -0500

    Revised Lab 8

--- Week 8/Hong_Kan_Poon_Lab8.sql
+++ Week 8/Hong_Kan_Poon_Lab8.sql
@@ -14,141 +14,83 @@ INSERT INTO librarians (librarian_fname, librarian_surname)
 		('Carla', 'Hayden'),
 		('Ardelia', 'Lortz'),
 		('Mary', 'Ahern'),
-		('Dolly', 'Parton')
-
-CREATE TABLE IF NOT EXISTS shifts (
-    shift_id INT PRIMARY KEY AUTO_INCREMENT,
-    shift INT NOT NULL
-);
-
-INSERT INTO shifts (shift)
-	VALUES
-		(1),
-		(2)
-		
-CREATE TABLE IF NOT EXISTS day (
-    day_id INT PRIMARY KEY AUTO_INCREMENT,
-    day DATE NOT NULL
-);
-
-INSERT INTO day (day)
-	VALUES
-		('2023-03-06'),
-		('2023-03-07'),
-		('2023-03-08'),
-		('2023-03-13'),
-		('2023-03-14'),
-		('2023-03-15')									
+		('Dolly', 'Parton')						
 
 CREATE TABLE IF NOT EXISTS assignments (
 	assignment_id INT PRIMARY KEY AUTO_INCREMENT,
-	assignment varchar(255) NOT NULL
+	day DATE,
+	shift ENUM ('morning','afternoon'),
+	assignment varchar(255)
 );
 
-INSERT INTO assignments (assignment)
+INSERT INTO assignments (day, shift, assignment)
 	VALUES
-		('main desk'),
-		('catalogue'),
-		('children''s library'),
-		('reference')
+		('2023-03-06',1,'main desk'),
+		('2023-03-06',1,'catalogue'),
+		('2023-03-06',2,'children''s library'),
+		('2023-03-06',1,'reference'),
+		('2023-03-06',1,'catalogue'),
+		('2023-03-07',1,'main desk'),
+		('2023-03-07',1,'reference'),
+		('2023-03-07',2,'main desk'),
+		('2023-03-07',2,'catalogue'),
+		('2023-03-08',1,'main desk'),
+		('2023-03-08',1,'catalogue'),
+		('2023-03-08',2,'reference'),
+		('2023-03-13',1,'main desk'),
+		('2023-03-13',1,'reference'),
+		('2023-03-13',1,'children''s library'),
+		('2023-03-13',1,'reference'),
+		('2023-03-13',1,'catalogue'),
+		('2023-03-14',1,'main desk'),
+		('2023-03-14',1,'children''s library'),
+		('2023-03-14',2,'main desk'),
+		('2023-03-14',2,'catalogue'),
+		('2023-03-15',1,'main desk'),
+		('2023-03-15',1,'catalogue')
 
-CREATE TABLE IF NOT EXISTS librarian_shift (
-	librarian_shift_id INT PRIMARY KEY AUTO_INCREMENT,
-    librarian_id INT,
-    shift_id INT,
-    CONSTRAINT librarian_id_fk_shift FOREIGN KEY (librarian_id) REFERENCES librarians(librarian_id) ON DELETE SET NULL,
-    CONSTRAINT shift_id_fk FOREIGN KEY (shift_id) REFERENCES shifts(shift_id) ON DELETE SET NULL
-);
-
-INSERT INTO librarian_shift (librarian_id, shift_id)
-	VALUES
-		(1,1),
-		(2,2),
-		(3,1),
-		(4,1),
-		(5,2),
-		(6,1),
-		(7,2),
-		(8,1),
-		(9,1),
-		(3,1),
-		(4,1),
-		(6,2),
-		(5,1)
-
-CREATE TABLE IF NOT EXISTS librarian_day (
-	librarian_day_id INT PRIMARY KEY AUTO_INCREMENT,
-    librarian_id INT,
-    day_id INT,
-    CONSTRAINT librarian_id_fk_day FOREIGN KEY (librarian_id) REFERENCES librarians(librarian_id) ON DELETE SET NULL,
-    CONSTRAINT day_id_fk FOREIGN KEY (day_id) REFERENCES day(day_id) ON DELETE SET NULL
-);
-
-INSERT INTO librarian_day (librarian_id, day_id)
-	VALUES
-		(1,1),
-		(2,1),
-		(3,1),
-		(4,2),
-	    (5,2),
-	    (6,3),
-	    (7,3),	    
-	    (8,4),
-	    (9,4),
-	    (3,4),
-	    (4,5),
-	    (6,5),
-	    (5,6)
-
-CREATE TABLE IF NOT EXISTS librarian_assignment (
-	librarian_assignment_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS duty (
+	duty_id INT PRIMARY KEY AUTO_INCREMENT,
     librarian_id INT,
     assignment_id INT,
     CONSTRAINT librarian_id_fk FOREIGN KEY (librarian_id) REFERENCES librarians(librarian_id) ON DELETE SET NULL,
     CONSTRAINT assignment_id_fk FOREIGN KEY (assignment_id) REFERENCES assignments(assignment_id) ON DELETE SET NULL
 );
 
-INSERT INTO librarian_assignment (librarian_id, assignment_id)
+INSERT INTO duty (librarian_id, assignment_id)
 	VALUES
 		(1,1),
 		(1,2),
 		(2,3),
 		(3,4),
-		(3,2),
-		(4,1),
-		(4,4),
-		(5,1),
-		(5,2),
-		(6,1),
-		(6,2),
-		(7,4),
-		(8,1),
-		(8,4),
-		(9,3),
-		(3,4),
-		(3,2),
-		(4,1),
-		(4,3),
-		(6,1),
-		(6,2),
-		(5,1),
-		(5,2)
+		(3,5),
+		(4,6),
+		(4,7),
+		(5,8),
+		(5,9),
+		(6,10),
+		(6,11),
+		(7,12),
+		(8,13),
+		(8,14),
+		(9,15),
+		(3,16),
+		(3,17),
+		(4,18),
+		(4,19),
+		(6,20),
+		(6,21),
+		(5,22),
+		(5,23)
 		
-SELECT DISTINCT librarian_fname, librarian_surname, shifts.shift
+SELECT DISTINCT librarian_fname , librarian_surname , assignments.shift
 FROM librarians
-INNER JOIN librarian_assignment ON librarians.librarian_id = librarian_assignment.librarian_id
-INNER JOIN assignments ON librarian_assignment.assignment_id = assignments.assignment_id
-INNER JOIN librarian_shift ON librarians.librarian_id = librarian_shift.librarian_id
-INNER JOIN shifts ON librarian_shift.shift_id = shifts.shift_id
-INNER JOIN librarian_day ON librarians.librarian_id = librarian_day.librarian_id
-INNER JOIN day ON librarian_day.day_id = day.day_id
-WHERE day.day = '2023-03-13'
+INNER JOIN duty ON librarians.librarian_id = duty.librarian_id
+INNER JOIN assignments ON duty.assignment_id = assignments.assignment_id
+WHERE assignments.day = '2023-03-13'
 
-SELECT *
+SELECT librarian_fname, librarian_surname, assignments.assignment
 FROM librarians
-LEFT JOIN librarian_assignment ON librarians.librarian_id = librarian_assignment.librarian_id
-LEFT JOIN assignments ON librarian_assignment.assignment_id = assignments.assignment_id
-LEFT JOIN librarian_shift ON librarians.librarian_id = librarian_shift.librarian_id
-LEFT JOIN shifts ON librarian_shift.shift_id = shifts.shift_id
-LEFT JOIN librarian_day ON librarians.librarian_id = librarian_day.librarian_id
-LEFT JOIN day ON librarian_day.day_id = day.day_id
+INNER JOIN duty ON librarians.librarian_id = duty.librarian_id
+INNER JOIN assignments ON duty.assignment_id = assignments.assignment_id
+WHERE assignments.day = '2023-03-13' AND librarian_fname = 'Beverly' AND librarian_surname = 'Cleary'
